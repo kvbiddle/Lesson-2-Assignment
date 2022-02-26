15102aaeee0db01acd964473f458d45dc209c157
kvbiddle <100012025+kvbiddle@users.noreply.github.com>
2022-02-26 16:10:53 +1100

    Add assignment

---
+++ lesson_2_annoying_text_assignment.py
@@ -0,0 +1,9 @@
+original_text = "strawberry "
+
+shouting_text = original_text.upper()
+
+annoying_text = 10 * shouting_text
+
+print(f"My annoying text is {annoying_text}")
+
+print(f"It has {len(annoying_text)} characters")
